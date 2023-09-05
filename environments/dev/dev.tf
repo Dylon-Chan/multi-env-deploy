f8909d0683bedf05fec4f5c1c353c3fc792099a9
Dylon-Chan <dylon_chan22@hotmail.com>
2023-09-05 23:02:54 +0800

    update

--- environments/dev/dev.tf
+++ environments/dev/dev.tf
@@ -31,7 +31,7 @@ resource "aws_ecs_task_definition" "task" {
   execution_role_arn = var.role_to_assume
   container_definitions = jsonencode([{
     name: "${var.ecs_name}-td",
-    image: "${var.image_name}:${var.image_tag}@${var.image_digest}",
+    image: "${var.image_name}:${var.image_tag}",
     cpu: var.cpu,
     portMappings: [
         {
@@ -44,6 +44,9 @@ resource "aws_ecs_task_definition" "task" {
 }])
 }
 
+# Generates a random UUID to be used as a trigger for the ECS service resource
+resource "random_uuid" "redeploy_trigger" {}
+
 resource "aws_ecs_service" "service" {
   name = "${var.ecs_name}-service"
   cluster = aws_ecs_cluster.cluster.id
@@ -52,6 +55,14 @@ resource "aws_ecs_service" "service" {
   depends_on = [ aws_security_group.ecs_sg, aws_ecs_task_definition.task ]
   desired_count = 1
 
+  force_new_deployment = true
+
+  #force the ECS service resource to be recreated or redeployed when thereis a change in the values inside this block.
+  # A unique hash combining the current timestamp and a random UUID is used as the value for the redeploy_trigger key.
+  # This ensures that the hash changes every time `terraform apply` is executed, causing the ECS service to redeploy due to the change in the 'triggers' value.
+  triggers = {
+    redeploy_trigger = sha1("${timestamp()}-${random_uuid.redeploy_trigger.result}")
+  }
   
   network_configuration {
     subnets = var.subnets
