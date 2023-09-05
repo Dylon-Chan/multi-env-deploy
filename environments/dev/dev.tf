b94ebd0f892cb463ca8812576e1144e540c8b9dd
Dylon-Chan <dylon_chan22@hotmail.com>
2023-09-05 19:27:48 +0800

    update

--- environments/dev/dev.tf
+++ environments/dev/dev.tf
@@ -51,11 +51,7 @@ resource "aws_ecs_service" "service" {
   launch_type = "FARGATE"
   depends_on = [ aws_security_group.ecs_sg, aws_ecs_task_definition.task ]
   desired_count = 1
-/*   force_new_deployment = true
 
-  triggers = {
-    redeployment = timestamp()
-  } */
   
   network_configuration {
     subnets = var.subnets
