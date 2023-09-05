20e8ab494e4869b619621f8bfe33c427205de717
Dylon-Chan <dylon_chan22@hotmail.com>
2023-09-06 00:21:37 +0800

    update

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -10,19 +10,3 @@ output "all_eni_id" {
   value = data.aws_network_interfaces.all.ids
 }
 
-data "aws_network_interface" "all" {
-  for_each = toset(data.aws_network_interfaces.all.ids)
-  id       = each.key
-}
-
-output "all_public_ip" {
-  value = {
-    for k, v in data.aws_network_interface.all : k => v.association[0].public_ip
-  }
-}
-
-output "all_access_urls" {
-  value = {
-    for k, v in data.aws_network_interface.all : k => "http://${v.association[0].public_ip}:${var.image_port}"
-  }
-}
